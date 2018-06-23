262ada8dae64b4f6d3270df145f7bf11642854f7
gunahao <30976902+zz6862532@users.noreply.github.com>
2018-06-23 15:40:38 +0800

    Add files via upload

---
+++ UserControl/User_Login.ascx
@@ -0,0 +1,45 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="User_Login.ascx.cs" Inherits="Design.UserControl.User_Login" %>
+
+<tr>
+  <td style="text-align:left">
+    <div style="text-align:center"> 
+        <table style="width: 211px;text-align:left">
+            <tr>
+                <td style="text-align:right;font-size:small">
+                    帐号：</td>
+                <td style="text-align:left">
+                    <asp:TextBox ID="UserName" runat="server" Width="113px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: small;text-align:right">
+                    密码：</td>
+                <td style="text-align:left">
+                    <asp:TextBox ID="UserPwd" runat="server" Width="114px" TextMode="Password" 
+                        Height="16px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: small;text-align:right">验证：</td>
+                <td style="font-size: x-small;text-align:left">
+                    <asp:TextBox ID="Validator" runat="server" Width="40px" Height="16px" ></asp:TextBox>  
+                    <img src="CreatePic.aspx" id="valiCode" alt="验证码" />
+                    <a title="刷新验证码" onclick="javascript:document.getElementById('valiCode').src='CreatePic.aspx?id='+Math.random();return false;">换图</a>
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align:center" colspan="2">
+                    <asp:ImageButton ID="imgbtnEnter" runat="server" ImageUrl="~/images/登录按钮.gif" 
+                        onclick="imgbtnEnter_Click" CausesValidation="False" />
+                    <asp:Label ID="lblMessage" runat="server" Font-Size="Small"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align:center" colspan="2">
+                    &nbsp;</td>
+            </tr>
+        </table>
+    
+    </div>
+            </td>
+   </tr>
---
+++ UserControl/User_Login.ascx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+namespace Design.UserControl
+{
+    public partial class User_Login : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void imgbtnEnter_Click(object sender, ImageClickEventArgs e)
+        {
+            if (Session["CheckCode"].ToString().ToLower() != Validator.Text.Trim().ToLower())
+            {
+                lblMessage.Text = "验证码错误！";
+            }
+            else
+            {
+                string username;
+                string password;
+                string MdPswdStr = FormsAuthentication.HashPasswordForStoringInConfigFile(UserPwd.Text.ToString(), "MD5");
+                username = UserName.Text.ToString();
+                password = MdPswdStr;
+                if (username.Equals("") || password.Equals(""))
+                {
+                    Response.Write("<script language=javascript>alert('会员名和密码不能为空！');location='javascript:history.go(-1)'</script>");
+                }
+                SqlConnection myconn = new SqlConnection(ConfigurationSettings.AppSettings["ConStr"].ToString());
+                string strsql = "select * from Users where UserName=@name and PassWord=@pass";
+                myconn.Open();
+                SqlCommand comm = new SqlCommand(strsql, myconn);
+                comm.Parameters.Add(new SqlParameter("name", SqlDbType.NVarChar, 50));
+                comm.Parameters["name"].Value = username;
+                comm.Parameters.Add(new SqlParameter("pass", SqlDbType.NVarChar, 50));
+                comm.Parameters["pass"].Value = password;
+                SqlDataReader dr = comm.ExecuteReader();
+                if (dr.Read())
+                {
+                    Session["username"] = username;
+                    string sql = "select count(*) from PerHomedetail where PerHomeUser='" + username + "'";
+                    DBConnection mydb = new DBConnection();
+                    if (mydb.ExecScalar(sql) > 0)
+                    {
+                        Response.Redirect("./Home_Manage.aspx");
+                    }
+                    else
+                    {
+                        Response.Redirect("./CreatPerHome.aspx");
+                    }
+                }
+                else
+                {
+                    Response.Write("<script language=javascript>alert('您输入的会员名或密码有错！请重新输入！');location='javascript:history.go(-1)'</script>");
+                }
+                dr.Close();
+                myconn.Close();
+            }
+        }
+    }
+}
